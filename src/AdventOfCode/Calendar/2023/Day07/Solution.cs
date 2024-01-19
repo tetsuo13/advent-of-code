6ec84c5df399c573088f1bf9755df3245a4feb67
Andrei Nicholson <contact@andreinicholson.com>
2024-01-18 20:41:37 -0500

    Solution for 2023-07 part 1

---
+++ src/AdventOfCode/Calendar/2023/Day07/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode.Calendar._2023.Day07;
+
+public class Solution : BaseSolution
+{
+    public override async Task<object> Run(RunMode runMode)
+    {
+        var hands = ParseInput(await ReadInput());
+
+        return runMode switch
+        {
+            RunMode.PartOne => TotalWinningsByStrength(hands),
+            RunMode.PartTwo => 0,
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    private static int TotalWinningsByStrength(IEnumerable<Hand> hands) =>
+        hands.Order()
+            .Select((hand, rank) => hand.Bid * (rank + 1))
+            .Sum();
+
+    private static IEnumerable<Hand> ParseInput(string[] input) =>
+        input.Select(line =>
+        {
+            var parts = line.Split(' ');
+            return new Hand(parts[0], Convert.ToInt32(parts[1]));
+        });
+}
---
+++ tests/AdventOfCode.Tests/Calendar/2023/Day07Tests.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode.Calendar._2023.Day07;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar._2023;
+
+public class Day07Tests
+{
+    [Fact]
+    public async Task Example()
+    {
+        string[] lines =
+            [
+                "32T3K 765",
+                "T55J5 684",
+                "KK677 28",
+                "KTJJT 220",
+                "QQQJA 483"
+            ];
+
+        var solution = new SolutionTestWrapper<Solution>(lines);
+        Assert.Equal(6440, await solution.Run(RunMode.PartOne));
+    }
+
+    [Theory]
+    [InlineData(RunMode.PartOne, 6592)]
+    //[InlineData(RunMode.PartTwo, 6839)]
+    public async Task Example2(RunMode runMode, int expected)
+    {
+        string[] lines =
+            [
+                "2345A 1",
+                "Q2KJJ 13",
+                "Q2Q2Q 19",
+                "T3T3J 17",
+                "T3Q33 11",
+                "2345J 3",
+                "J345A 2",
+                "32T3K 5",
+                "T55J5 29",
+                "KK677 7",
+                "KTJJT 34",
+                "QQQJA 31",
+                "JJJJJ 37",
+                "JAAAA 43",
+                "AAAAJ 59",
+                "AAAAA 61",
+                "2AAAA 23",
+                "2JJJJ 53",
+                "JJJJ2 41"
+            ];
+
+        var solution = new SolutionTestWrapper<Solution>(lines);
+        Assert.Equal(expected, await solution.Run(runMode));
+    }
+}

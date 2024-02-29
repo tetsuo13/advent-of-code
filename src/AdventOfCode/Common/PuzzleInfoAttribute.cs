60c17bdc86436b0846d38e9d0aa20e21ee389b4c
Andrei Nicholson <contact@andreinicholson.com>
2024-02-28 21:25:16 -0500

    Fix missing file from previous commit

---
+++ src/AdventOfCode/Common/PuzzleInfoAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Common;
+
+[AttributeUsage(AttributeTargets.Class)]
+internal class PuzzleInfoAttribute(int year, int day, string name) : Attribute
+{
+    public int Year { get; } = year;
+    public int Day { get; } = day;
+    public string Name { get; } = name;
+}

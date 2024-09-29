d67325fbf83405a22bb90cf3cb648bd383440420
Andrei Nicholson <contact@andreinicholson.com>
2024-09-28 20:14:44 -0400

    WIP 2023-10

---
+++ src/AdventOfCode/Calendar/Year2023/Day10/Solution.cs
@@ -0,0 +1,72 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Calendar.Year2023.Day10;
+
+[PuzzleInfo(2023, 10, "Pipe Maze")]
+public class Solution : BaseSolution
+{
+    private readonly record struct Point(long X, long Y);
+
+    private const string StartingPosition = "S";
+
+    public override async Task<object> Run(RunMode runMode)
+    {
+        var lines = await ReadInput();
+        var field = CreateFieldGrid(lines);
+
+        return runMode switch
+        {
+            RunMode.PartOne => FarthestFromStartingPosition(field),
+            RunMode.PartTwo => 0,
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    private static int FarthestFromStartingPosition(string[,] field)
+    {
+        var startingPosition = FindStartingPosition(field);
+        var steps = 0;
+
+        var next = GetNextSignificantPosition(field, startingPosition);
+
+        return steps;
+    }
+
+    private static Point GetNextSignificantPosition(string[,] field, Point currentPosition)
+    {
+        return new Point(1, 1);
+    }
+
+    private static Point FindStartingPosition(string[,] field)
+    {
+        for (var x = 0; x < field.GetLength(0); x++)
+        {
+            for (var y = 0; y < field.GetLength(1); y++)
+            {
+                if (field[x, y] == StartingPosition)
+                {
+                    return new Point(x, y);
+                }
+            }
+        }
+
+        throw new InvalidDataException("Field missing starting position");
+    }
+
+    private static string[,] CreateFieldGrid(IReadOnlyList<string> lines)
+    {
+        var width = lines[0].Length;
+        var height = lines.Count;
+        string[,] field = new string[width, height];
+
+        for (var i = 0; i < height; i++)
+        {
+            for (var j = 0; j < width; j++)
+            {
+                field[i, j] = lines[i][j].ToString();
+            }
+        }
+
+        return field;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Calendar/Year2023/Day10Tests.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode.Calendar.Year2023.Day10;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar.Year2023;
+
+public class Day10Tests
+{
+    [Fact(Skip = "WIP")]
+    public async Task Example1Simple()
+    {
+        string[] lines =
+            [
+                ".....",
+                ".S-7.",
+                ".|.|.",
+                ".L-J.",
+                "....."
+            ];
+
+        var solution = new SolutionTestWrapper<Solution>(lines);
+        Assert.Equivalent(4, await solution.Run(RunMode.PartOne));
+    }
+
+    [Fact(Skip = "WIP")]
+    public async Task Example1Complex()
+    {
+        string[] lines =
+            [
+                "-L|F7",
+                "7S-7|",
+                "L|7||",
+                "-L-J|",
+                "L|-JF"
+            ];
+
+        var solution = new SolutionTestWrapper<Solution>(lines);
+        Assert.Equivalent(4, await solution.Run(RunMode.PartOne));
+    }
+
+    [Fact(Skip = "WIP")]
+    public async Task Example2()
+    {
+        string[] lines =
+            [
+                "..F7.",
+                ".FJ|.",
+                "SJ.L7",
+                "|F--J",
+                "LJ..."
+            ];
+
+        var solution = new SolutionTestWrapper<Solution>(lines);
+        Assert.Equivalent(8, await solution.Run(RunMode.PartOne));
+    }
+}

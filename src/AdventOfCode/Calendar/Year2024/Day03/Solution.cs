ba42918fc2c77835750674a76f13128c401cb46b
Andrei Nicholson <contact@andreinicholson.com>
2024-12-07 21:20:20 -0500

    Solution for 2024-12-03

---
+++ src/AdventOfCode/Calendar/Year2024/Day03/Solution.cs
@@ -0,0 +1,91 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Calendar.Year2024.Day03;
+
+/// <summary>
+/// Avoiding regular expressions because that would be too easy.
+/// </summary>
+[PuzzleInfo(2024, 3, "Mull It Over")]
+public class Solution : BaseSolution
+{
+    public override object Run(RunMode runMode)
+    {
+        var inputLines = ReadInput();
+
+        return runMode switch
+        {
+            RunMode.PartOne => SumMulInstructions(inputLines, false),
+            RunMode.PartTwo => SumMulInstructions(inputLines, true),
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    private static int SumMulInstructions(string[] lines, bool useConditions)
+    {
+        var sum = 0;
+
+        // mul instructions should be enabled at the start of the program.
+        var enableMulInstructions = true;
+
+        foreach (var line in lines)
+        {
+            var mul = line.Split("mul");
+
+            foreach (var instruction in mul)
+            {
+                var mulInstructionResult = enableMulInstructions ? ParseMulInstruction(instruction) : null;
+
+                if (mulInstructionResult is not null)
+                {
+                    sum += mulInstructionResult.Value;
+                }
+
+                if (!useConditions)
+                {
+                    continue;
+                }
+
+                if (instruction.LastIndexOf("don't()", StringComparison.InvariantCulture) != -1)
+                {
+                    enableMulInstructions = false;
+                }
+                else if (instruction.LastIndexOf("do()", StringComparison.InvariantCulture) != -1)
+                {
+                    enableMulInstructions = true;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    private static int? ParseMulInstruction(string instruction)
+    {
+        if (instruction[0] != '(')
+        {
+            return null;
+        }
+
+        var parts = instruction.Split(',');
+
+        if (!int.TryParse(parts[0][1..], out var x))
+        {
+            return null;
+        }
+
+        var closingPos = parts[1].IndexOf(')');
+
+        // No closing parenthesis or number is greater than 3 digits.
+        if (closingPos is -1 or > 3)
+        {
+            return null;
+        }
+
+        if (!int.TryParse(parts[1][..closingPos], out var y))
+        {
+            return null;
+        }
+
+        return x * y;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Calendar/Year2024/Day03Tests.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode.Calendar.Year2024.Day03;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar.Year2024;
+
+public class Day03Tests
+{
+    [Fact]
+    public void PartOne()
+    {
+        string[] input = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(161, solution.Run(RunMode.PartOne));
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        string[] input = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(48, solution.Run(RunMode.PartTwo));
+    }
+
+    [Fact]
+    public void PartTwo_DontInstructionWithoutAnyFollowingDoInstructions()
+    {
+        string[] input = ["'{how()when()<@,}mul(512,183),']why()(who()&<when()~don't()who()~)-why(899,149)how()mul(931,445)how(852,127)"];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(93_696, solution.Run(RunMode.PartTwo));
+    }
+}

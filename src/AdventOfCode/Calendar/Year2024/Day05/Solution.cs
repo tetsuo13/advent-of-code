2a70d80cb8b03f6de5093f83b1531b2d8f49a862
Andrei Nicholson <contact@andreinicholson.com>
2024-12-08 21:43:08 -0500

    Solution for 2024-12-05 part 1 (first pass)

---
+++ src/AdventOfCode/Calendar/Year2024/Day05/Solution.cs
@@ -0,0 +1,89 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Calendar.Year2024.Day05;
+
+[PuzzleInfo(2024, 5, "Print Queue")]
+public class Solution : BaseSolution
+{
+    public override object Run(RunMode runMode)
+    {
+        var inputLines = ReadInput();
+        var (pageOrderingRules, sectionTwoLine) = ParsePageOrderingRules(inputLines);
+
+        return runMode switch
+        {
+            RunMode.PartOne => SumMiddlePageNumberOfCorrectlyOrderedUpdates(inputLines, pageOrderingRules, sectionTwoLine),
+            RunMode.PartTwo => 0,
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    private static int SumMiddlePageNumberOfCorrectlyOrderedUpdates(string[] inputLines,
+        Dictionary<int, List<int>> pageOrderingRules, int sectionTwoLine)
+    {
+        var sum = 0;
+
+        for (var i = sectionTwoLine; i < inputLines.Length; i++)
+        {
+            var pageNumbers = inputLines[i].Split(',').Select(int.Parse).ToList();
+
+            if (!IsCorrectOrder(pageOrderingRules, pageNumbers))
+            {
+                continue;
+            }
+
+            sum += pageNumbers[pageNumbers.Count / 2];
+        }
+
+        return sum;
+    }
+
+    private static bool IsCorrectOrder(Dictionary<int, List<int>> pageOrderingRules, List<int> pageNumbers)
+    {
+        var firstPass = pageNumbers[1..].All(pageOrderingRules[pageNumbers[0]].Contains);
+
+        if (!firstPass)
+        {
+            return false;
+        }
+
+        for (var j = 0; j < pageNumbers.Count - 1; j++)
+        {
+            if (!pageOrderingRules.TryGetValue(pageNumbers[j], out List<int>? value) ||
+                !value.Contains(pageNumbers[j + 1]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static (Dictionary<int, List<int>>, int) ParsePageOrderingRules(string[] inputLines)
+    {
+        var pageOrderingRules = new Dictionary<int, List<int>>();
+        var i = 0;
+
+        for (; i < inputLines.Length; i++)
+        {
+            if (string.IsNullOrEmpty(inputLines[i]))
+            {
+                break;
+            }
+
+            var pageX = int.Parse(inputLines[i][..2]);
+            var pageY = int.Parse(inputLines[i][3..]);
+
+            if (pageOrderingRules.TryGetValue(pageX, out List<int>? value))
+            {
+                value.Add(pageY);
+            }
+            else
+            {
+                pageOrderingRules.Add(pageX, [pageY]);
+            }
+        }
+
+        return (pageOrderingRules, i + 1);
+    }
+}
---
+++ tests/AdventOfCode.Tests/Calendar/Year2024/Day05Tests.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode.Calendar.Year2024.Day05;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar.Year2024;
+
+public class Day05Tests
+{
+    private readonly string[] _input =
+    [
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47"
+    ];
+
+    [Theory]
+    [InlineData(RunMode.PartOne, 143)]
+    public void Example(RunMode runMode, int expected)
+    {
+        var solution = new SolutionTestWrapper<Solution>(_input);
+        Assert.Equal(expected, solution.Run(runMode));
+    }
+}

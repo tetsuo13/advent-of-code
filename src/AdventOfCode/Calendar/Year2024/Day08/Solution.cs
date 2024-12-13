7fccdfd21b9a9856430e386626cd531c669353b9
Andrei Nicholson <contact@andreinicholson.com>
2024-12-13 07:53:29 -0500

    Solution for 2024-12-08

---
+++ src/AdventOfCode/Calendar/Year2024/Day08/Solution.cs
@@ -0,0 +1,122 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Calendar.Year2024.Day08;
+
+[PuzzleInfo(2024, 8, "Resonant Collinearity")]
+public class Solution : BaseSolution
+{
+    public override object Run(RunMode runMode)
+    {
+        var inputLines = ReadInput();
+
+        return runMode switch
+        {
+            RunMode.PartOne => LocationsWithAntinodes(inputLines, false),
+            RunMode.PartTwo => LocationsWithAntinodes(inputLines, true),
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    // TODO: Move this out
+    private readonly record struct Point(int X, int Y);
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="inputLines"></param>
+    /// <param name="accountForResonantHarmonics">
+    /// When disabled, will only one antinode from an antenna. When enabled,
+    /// will create as many antinodes as will fit in the map.
+    /// </param>
+    /// <returns></returns>
+    private static int LocationsWithAntinodes(string[] inputLines, bool accountForResonantHarmonics)
+    {
+        var antinodes = new List<Point>();
+        var antennas = FindAllAntennasByFrequency(inputLines);
+
+        // Arbitrary upper bound number used -- mostly randomly, chosen to be
+        // large enough to accomodate the puzzle input map size.
+        var distance = accountForResonantHarmonics ? 40 : 1;
+
+        foreach ((char _, List<Point> positions) in antennas)
+        {
+            // Compare each antenna with every other antenna of the same
+            // frequency.
+            for (var i = 0; i < positions.Count; i++)
+            {
+                if (accountForResonantHarmonics)
+                {
+                    // The antenna itself is an antinode.
+                    antinodes.Add(positions[i]);
+                }
+
+                for (var j = i + 1; j < positions.Count; j++)
+                {
+                    var rise = positions[j].Y - positions[i].Y;
+                    var run = positions[j].X - positions[i].X;
+
+                    var increasingAntinodes = new List<Point>();
+
+                    for (var multiplier = 1; multiplier <= distance; multiplier++)
+                    {
+                        var antinode = new Point(positions[j].X + run * multiplier, positions[j].Y + rise * multiplier);
+
+                        if (IsPositionWithinMap(inputLines, antinode))
+                        {
+                            increasingAntinodes.Add(antinode);
+                        }
+                    }
+
+                    antinodes.AddRange(increasingAntinodes);
+
+                    var decreasingAntinodes = new List<Point>();
+
+                    for (var multiplier = 1; multiplier <= distance; multiplier++)
+                    {
+                        var antinode = new Point(positions[i].X - run * multiplier, positions[i].Y - rise * multiplier);
+
+                        if (IsPositionWithinMap(inputLines, antinode))
+                        {
+                            decreasingAntinodes.Add(antinode);
+                        }
+                    }
+
+                    antinodes.AddRange(decreasingAntinodes);
+                }
+            }
+        }
+
+        return antinodes.Distinct().Count();
+    }
+
+    private static bool IsPositionWithinMap(string[] inputLines, Point position) =>
+        position.X >= 0 && position.X < inputLines[0].Length &&
+        position.Y >= 0 && position.Y < inputLines.Length;
+
+    private static Dictionary<char, List<Point>> FindAllAntennasByFrequency(string[] inputLines)
+    {
+        var antennas = new Dictionary<char, List<Point>>();
+
+        for (var i = 0; i < inputLines.Length; i++)
+        {
+            for (var j = 0; j < inputLines[i].Length; j++)
+            {
+                // Only look at stations.
+                if (inputLines[i][j] == '.')
+                {
+                    continue;
+                }
+
+                if (!antennas.TryGetValue(inputLines[i][j], out List<Point>? frequencyPositions))
+                {
+                    frequencyPositions = [];
+                    antennas.Add(inputLines[i][j], frequencyPositions);
+                }
+
+                frequencyPositions.Add(new Point(j, i));
+            }
+        }
+
+        return antennas;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Calendar/Year2024/Day08Tests.cs
@@ -0,0 +1,112 @@
+﻿using AdventOfCode.Calendar.Year2024.Day08;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar.Year2024;
+
+public class Day08Tests
+{
+    [Fact]
+    public void PartOne_Example1()
+    {
+        string[] input =
+        [
+            "..........",
+            "..........",
+            "..........",
+            "....a.....",
+            "..........",
+            ".....a....",
+            "..........",
+            "..........",
+            "..........",
+            ".........."
+        ];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(2, solution.Run(RunMode.PartOne));
+    }
+
+    [Fact]
+    public void PartOne_Example2()
+    {
+        string[] input =
+        [
+            "..........",
+            "..........",
+            "..........",
+            "....a.....",
+            "........a.",
+            ".....a....",
+            "..........",
+            "..........",
+            "..........",
+            ".........."
+        ];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(4, solution.Run(RunMode.PartOne));
+    }
+
+    [Fact]
+    public void PartOne_Example3()
+    {
+        string[] input =
+        [
+            "..........",
+            "..........",
+            "..........",
+            "....a.....",
+            "........a.",
+            ".....a....",
+            "..........",
+            "......A...",
+            "..........",
+            ".........."
+        ];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(4, solution.Run(RunMode.PartOne));
+    }
+
+    [Theory]
+    [InlineData(RunMode.PartOne, 14)]
+    [InlineData(RunMode.PartTwo, 34)]
+    public void Example(RunMode runMode, int expected)
+    {
+        string[] input =
+        [
+            "............",
+            "........0...",
+            ".....0......",
+            ".......0....",
+            "....0.......",
+            "......A.....",
+            "............",
+            "............",
+            "........A...",
+            ".........A..",
+            "............",
+            "............"
+        ];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(expected, solution.Run(runMode));
+    }
+
+    [Fact]
+    public void PartTwo_Example1()
+    {
+        string[] input =
+        [
+            "T.........",
+            "...T......",
+            ".T........",
+            "..........",
+            "..........",
+            "..........",
+            "..........",
+            "..........",
+            "..........",
+            ".........."
+        ];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(9, solution.Run(RunMode.PartTwo));
+    }
+}

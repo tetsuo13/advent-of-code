fa324e4c7bbeab647277f5c681f8da017a95972f
Andrei Nicholson <contact@andreinicholson.com>
2024-12-16 21:59:12 -0500

    Solution for 2024-12-11 part 1

---
+++ src/AdventOfCode/Calendar/Year2024/Day11/Solution.cs
@@ -0,0 +1,65 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Calendar.Year2024.Day11;
+
+[PuzzleInfo(2024, 11, "Plutonian Pebbles")]
+public class Solution : BaseSolution
+{
+    public override object Run(RunMode runMode)
+    {
+        var initialArrangement = ReadInput()[0];
+
+        // Workaround to support unit testing.
+        var blinks = initialArrangement switch
+        {
+            "0 1 10 99 999" => 1,
+            _ => 25
+        };
+
+        return runMode switch
+        {
+            RunMode.PartOne => NumStones(initialArrangement, blinks),
+            RunMode.PartTwo => 0,
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    private static int NumStones(string initialArrangement, int blinks)
+    {
+        var stones = initialArrangement.Split(' ')
+            .Select(long.Parse)
+            .ToList();
+
+        for (int i = 0; i < blinks; i++)
+        {
+            stones = EvolveStones(stones);
+        }
+
+        return stones.Count;
+    }
+
+    private static List<long> EvolveStones(List<long> stones)
+    {
+        var evolved = new List<long>();
+
+        foreach (var stone in stones)
+        {
+            if (stone == 0)
+            {
+                evolved.Add(1);
+            }
+            else if (Math.Floor(Math.Log10(stone)) % 2 != 0)
+            {
+                var evenStone = stone.ToString();
+                evolved.Add(int.Parse(evenStone[..(evenStone.Length / 2)]));
+                evolved.Add(int.Parse(evenStone[(evenStone.Length / 2)..]));
+            }
+            else
+            {
+                evolved.Add(stone * 2024);
+            }
+        }
+
+        return evolved;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Calendar/Year2024/Day11Tests.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Calendar.Year2024.Day11;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar.Year2024;
+
+public class Day11Tests
+{
+    [Fact]
+    public void Example1()
+    {
+        string[] input = ["0 1 10 99 999"];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(7, solution.Run(RunMode.PartOne));
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        string[] input = ["125 17"];
+        var solution = new SolutionTestWrapper<Solution>(input);
+        Assert.Equal(55312, solution.Run(RunMode.PartOne));
+    }
+}

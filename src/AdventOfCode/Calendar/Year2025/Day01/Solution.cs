d3af39865419b321119e8bc62e3b1f2eaec2d230
Andrei Nicholson <contact@andreinicholson.com>
2025-12-02 21:49:54 -0500

    Solution for 2025 day 1

---
+++ src/AdventOfCode/Calendar/Year2025/Day01/Solution.cs
@@ -0,0 +1,80 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Calendar.Year2025.Day01;
+
+[PuzzleInfo(2025, 1, "Secret Entrance")]
+public class Solution : BaseSolution
+{
+    public override object Run(RunMode runMode)
+    {
+        var rotations = ReadInput();
+
+        return runMode switch
+        {
+            RunMode.PartOne => DialPointedToZero(rotations, false),
+            RunMode.PartTwo => DialPointedToZero(rotations, true),
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="rotations">
+    /// The collection of rotations. Each element denotes the direction and
+    /// distance of a single rotation. For example, "L68" means rotate left 68
+    /// clicks.
+    /// </param>
+    /// <param name="utilizePasswordMethod0x434C49434B">
+    /// When <see langword="true"/>, count every time the dial touches zero.
+    /// For example, if the dial is at 50, a rotation of L68 will end up
+    /// pointing to 82. During the rotation it passed through zero once. So
+    /// it's counted once.
+    /// <para/>
+    /// When <see langword="false"/>, count every time the dial points to 0
+    /// after a rotation. Using the same example as before, it's not counted.
+    /// </param>
+    private static int DialPointedToZero(string[] rotations, bool utilizePasswordMethod0x434C49434B)
+    {
+        var numZeroes = 0;
+        var dial = 50;
+
+        foreach (var rotation in rotations)
+        {
+            var previous = dial;
+            var direction = rotation[0] switch
+            {
+                'L' => -1,
+                'R' => 1,
+                _ => throw new ArgumentOutOfRangeException($"Encountered unknown direction: {rotation[0]}")
+            };
+
+            dial += direction * int.Parse(rotation[1..]);
+
+            if (utilizePasswordMethod0x434C49434B)
+            {
+                // After rotation. Check if the dial landed on zero or passed
+                // through zero as indicated by a sign change of the dial: if
+                // it was previously positive and now negative, or vice versa.
+                if (dial == 0 || previous * dial < 0)
+                {
+                    numZeroes++;
+                }
+
+                // Treat 100 as zero
+                numZeroes += Math.Abs(dial / 100);
+                dial %= 100;
+            }
+            else
+            {
+                dial %= 100;
+
+                if (dial == 0)
+                {
+                    numZeroes++;
+                }
+            }
+        }
+
+        return numZeroes;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Calendar/Year2025/Day01Tests.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode.Calendar.Year2025.Day01;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar.Year2025;
+
+public class Day01Tests
+{
+    private readonly string[] _input =
+    [
+        "L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82"
+    ];
+
+    [Theory]
+    [InlineData(RunMode.PartOne, 3)]
+    [InlineData(RunMode.PartTwo, 6)]
+    public void Example(RunMode runMode, int expected)
+    {
+        var solution = new SolutionTestWrapper<Solution>(_input);
+        Assert.Equal(expected, solution.Run(runMode));
+    }
+}

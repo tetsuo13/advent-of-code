468288a05087c9d99029c6ea2a4c7104fa186d16
Andrei Nicholson <contact@andreinicholson.com>
2025-12-04 13:02:24 -0500

    Solution for 2025 day 2 part 1
    Naive brute force approach. I'd bet part 2 will be painful.

---
+++ src/AdventOfCode/Calendar/Year2025/Day02/Solution.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Calendar.Year2025.Day02;
+
+[PuzzleInfo(2025, 2, "Gift Shop")]
+public class Solution : BaseSolution
+{
+    public override object Run(RunMode runMode)
+    {
+        var rotations = ReadInput();
+
+        return runMode switch
+        {
+            RunMode.PartOne => SumInvalidIds(rotations[0]),
+            RunMode.PartTwo => 0,
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    private static long SumInvalidIds(string idRanges)
+    {
+        long sum = 0;
+
+        foreach (var range in idRanges.Split(','))
+        {
+            var firstId = long.Parse(range[..range.IndexOf('-')]);
+            var lastId = long.Parse(range[(range.IndexOf('-') + 1)..]);
+
+            // Brute force approach to look at every number.
+            for (var i = firstId; i <= lastId; i++)
+            {
+                var numDigits = Math.Floor(1 + Math.Log10(i));
+
+                // Only look at numbers that contain an even number of digits.
+                // Odd numbers can't be valid.
+                if (numDigits % 2 != 0)
+                {
+                    continue;
+                }
+
+                // Treating the number as a string, compare the first half
+                // with the last half for equality.
+                var id = i.ToString();
+
+                if (id[..(id.Length / 2)] == id[(id.Length / 2)..])
+                {
+                    sum += i;
+                }
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Calendar/Year2025/Day02Tests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode.Calendar.Year2025.Day02;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar.Year2025;
+
+public class Day02Tests
+{
+    private readonly string[] _input =
+    [
+        "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    ];
+
+    [Theory]
+    [InlineData(RunMode.PartOne, 1_227_775_554)]
+    public void Example(RunMode runMode, long expected)
+    {
+        var solution = new SolutionTestWrapper<Solution>(_input);
+        Assert.Equal(expected, solution.Run(runMode));
+    }
+}

0990315ad5d2513e36438fd8e1fd4e71d0c3bda3
Andrei Nicholson <contact@andreinicholson.com>
2025-12-08 16:13:34 -0500

    Solution for 2025 day 3 part 1

---
+++ src/AdventOfCode/Calendar/Year2025/Day03/Solution.cs
@@ -0,0 +1,65 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Calendar.Year2025.Day03;
+
+[PuzzleInfo(2025, 3, "Lobby")]
+public class Solution : BaseSolution
+{
+    public override object Run(RunMode runMode)
+    {
+        var banks = ReadInput();
+
+        return runMode switch
+        {
+            RunMode.PartOne => TotalOutputJoltage(banks),
+            RunMode.PartTwo => 0,
+            _ => throw new ArgumentOutOfRangeException(nameof(runMode))
+        };
+    }
+
+    private static double TotalOutputJoltage(string[] banks)
+    {
+        double sum = 0;
+
+        foreach (var bank in banks)
+        {
+            int firstBattery = -1;
+            int secondBattery = -1;
+
+            // Use ASCII codes to avoid extra conversions.
+            for (var i = 57; i > 49; i--)
+            {
+                firstBattery = bank.IndexOf((char)i);
+
+                if (firstBattery != -1 && firstBattery < bank.Length - 1)
+                {
+                    break;
+                }
+            }
+
+            if (firstBattery == -1)
+            {
+                throw new ArithmeticException("First battery wasn't found. This should not happen.");
+            }
+
+            for (var i = 57; i > 49; i--)
+            {
+                secondBattery = bank.IndexOf((char)i, firstBattery + 1);
+
+                if (secondBattery != -1)
+                {
+                    break;
+                }
+            }
+
+            if (secondBattery == -1)
+            {
+                throw new ArithmeticException("Second battery wasn't found. This should not happen.");
+            }
+
+            sum += char.GetNumericValue(bank[firstBattery]) * 10 + char.GetNumericValue(bank[secondBattery]);
+        }
+
+        return sum;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Calendar/Year2025/Day03Tests.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Calendar.Year2025.Day03;
+using AdventOfCode.Common;
+using Xunit;
+
+namespace AdventOfCode.Tests.Calendar.Year2025;
+
+public class Day03Tests
+{
+    private readonly string[] _input =
+    [
+        "987654321111111",
+        "811111111111119",
+        "234234234234278",
+        "818181911112111"
+    ];
+
+    [Theory]
+    [InlineData(RunMode.PartOne, 357)]
+    public void Example(RunMode runMode, double expected)
+    {
+        var solution = new SolutionTestWrapper<Solution>(_input);
+        Assert.Equal(expected, solution.Run(runMode));
+    }
+}
